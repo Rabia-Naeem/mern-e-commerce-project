35d11f328773cf6ff2bccf593933a47532152fc6
Rabia <rabianaeem1998@gmail.com>
2024-06-19 20:55:11 +0500

    getting products from the datatbase

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
-
+import productRoutes from "./routes/productRoutes.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
@@ -13,13 +12,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => console.log("listening on port ", port));
